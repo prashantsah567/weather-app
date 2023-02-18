4f5b5628c286dca405ff8b500e72a8dbbae54e1c
Prashant Sah <prashantsah567@gmail.com>
2023-02-17 19:37:05 -0500

    current weather setup with hidden part included

--- src/index.js
+++ src/index.js
@@ -4,16 +4,16 @@ import myImage from './assets/images/clear-day.jpg';
 const input = document.getElementById('myInput');
 
 //set background color
-//document.body.style.backgroundColor = "#94b8b8";
+document.body.style.backgroundColor = "#94b8b8";
 
 //using the imported image 
 const img = new Image();
 img.src = myImage;
-document.body.setAttribute("style",`background-image:url(${img.src}); background-size:cover; background-repeat:no-repeat;`);
 
 input.addEventListener("keydown", (event) =>{
     if(event.key === "Enter"){
         getData();
+        $(".hidden-part").show();
     }
 });
 
@@ -26,26 +26,31 @@ async function getData() {
         console.log(data);
 
         //assigning all the data fields for current condition
-        $("#condition").html(data.currentConditions.conditions);
+        $("#condition").html(data.days[0].conditions);
         $("#address").html(data.resolvedAddress);
-        $("#time").html(data.currentConditions.datetime);
+        $("#date").html(data.days[0].datetime);
         $("#latitude").html(data.latitude);
         $("#longitude").html(data.longitude);
-        $("#temp").html(data.currentConditions.feelslike);
+        $("#temp").html(data.currentConditions.temp);
+        $("#feelslike").html(data.currentConditions.feelslike);
+        $("#sunrise").html(data.currentConditions.sunrise);
+        $("#sunset").html(data.currentConditions.sunset);
         $("#humidity").html(data.currentConditions.humidity);
-        $("#weatherDesc").html(data.description);
-        //for day-1
-        $("#temp-max").html(data.days[0].feelslikemax);
-        $("#temp-min").html(data.days[0].feelslikemin);
-        
+        $("#visibility").html(data.currentConditions.visibility);
+        $("#windSpeed").html(data.currentConditions.windspeed);
+        $("#weatherDesc").html(data.days[0].description);
+
 
         //api calls for background image change
-        const fetchImg = await fetch(`https://pixabay.com/api/?key=32846660-95d8df5d4035034b605954169&q=${data.days[0].icon}&image_type=photo`,{mod: 'cors'});
-        const imgData = await fetchImg.json();
-        console.log(imgData);
-        const imgURL = imgData.hits[Math.floor(Math.random() * 20)].largeImageURL;
-        document.body.setAttribute("style",`background-image:url(${imgURL}); background-size:cover; background-repeat:no-repeat;`);
+        // const fetchImg = await fetch(`https://pixabay.com/api/?key=32846660-95d8df5d4035034b605954169&q=${data.days[0].icon}&image_type=photo`,{mod: 'cors'});
+        // const imgData = await fetchImg.json();
+        // console.log(imgData);
+        // const imgURL = imgData.hits[Math.floor(Math.random() * 20)].largeImageURL;
+        // document.body.setAttribute("style",`background-image:url(${imgURL}); background-size:cover; background-repeat:no-repeat;`);
     
+        //setting custom image
+        document.body.setAttribute("style",`background-image:url(${img.src}); background-size:cover; background-repeat:no-repeat;`);
+
     }catch(err){
         $("#errId").html("City not found");
     }  
